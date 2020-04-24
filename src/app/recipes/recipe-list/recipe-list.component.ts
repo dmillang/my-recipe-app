6448b681000bc6aee23da8cea2febd61160462f4
David Millan <hello@davidmillan.me>
2020-04-24 23:53:26 +0100

    Add content to the recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','Just a test','https://p1.pxfuel.com/preview/790/544/941/recipes-burgers-vegetables-burger.jpg')
+  ];
 
   constructor() { }
 
